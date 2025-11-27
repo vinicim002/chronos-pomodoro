0f3fe29dc8d0e5bf7eb76bd4d96f3294aa9669a5
Vinicius Silva <viniciusvilanova7@gmail.com>
2025-11-26 21:17:32 -0300

    Criando o formulário da página inicia

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 
@@ -19,8 +18,27 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+
       <Container>
-        <Heading>FOOTER</Heading>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='input'>task</label>
+            <input id='input' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
