3f97d5057ae8e254913a7d5f7c2df205b7220902
Vinicius Silva <viniciusvilanova7@gmail.com>
2025-12-03 10:11:59 -0300

    Context API - movendo nosso estado real para dentro do contexto

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './styles/theme.css';
 import './styles/global.css';
 import { useState } from 'react';
 import type { TaskStateModel } from './models/TaskStateModel';
-import { TaskContext } from './contexts/TaskContext';
+import { TaskContextProvider } from './contexts/TaskContext';
 
 const initialState: TaskStateModel = {
   tasks: [],
@@ -25,8 +25,8 @@ export function App() {
   const [state, setState] = useState(initialState);
 
   return (
-    <TaskContext.Provider value={{ outraCoisa: 123 }}>
+    <TaskContextProvider>
       <Home />;
-    </TaskContext.Provider>
+    </TaskContextProvider>
   );
 }
--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,5 +1,43 @@
-import { createContext } from "react";
+import { Children, createContext, useContext } from 'react';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 
-export const TaskContext = createContext({
-  chave: 'valor',
-});
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '05:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25 * 60,
+    shortBreakTime: 5 * 60,
+    longBreakTime: 15 * 60,
+  },
+};
+
+type TaskContextProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+const initialContextValue = {
+  state: initialState,
+  setState: () => {},
+};
+
+export const TaskContext = createContext<TaskContextProps>(initialContextValue);
+
+type TaskContextProviderProps = {
+  children: React.ReactNode;
+};
+
+export function TaskContextProvider({ children }: TaskContextProviderProps) {
+  return (
+    <TaskContext.Provider value={initialContextValue}>
+      {children}
+    </TaskContext.Provider>
+  );
+}
+
+export function useTaskContext() {
+  return useContext(TaskContext);
+}
