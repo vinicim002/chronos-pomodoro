ecb407651a84525a446a511b0e4a28f714b69dc2
Vinicius Silva <viniciusvilanova7@gmail.com>
2025-12-04 11:08:37 -0300

    Validando o formulário e criando uma nova tarefa

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,8 +3,11 @@ import { Input } from '../Input';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
   //Forma Controlada -> quando quero o valor do input em tempo real
   // const [taskName, setTaskName] = useState('');
 
@@ -13,7 +16,39 @@ export function MainForm() {
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Por favor, insira uma tarefa válida.');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completedDate: null,
+      interruptedDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, //Conferir depois
+        secondsRemaining: secondsRemaining, //Conferir depois
+        formattedSecondsRemaining: '00:00', //Conferir depois
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
