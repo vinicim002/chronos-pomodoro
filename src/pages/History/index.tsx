2ec454f1a35e986521f1de31e9351a4a5f723a1a
Vinicius Silva <viniciusvilanova7@gmail.com>
2025-12-11 11:51:01 -0300

    Criando a estrutura e CSS da página History (Histórico)

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -14,13 +14,43 @@ export function History() {
           <Heading>
             <span>History</span>
             <span className={styles.buttonContainer}>
-              <DefaultButton icon={<TrashIcon />} color='red' />
+              <DefaultButton
+                icon={<TrashIcon />}
+                color='red'
+                aria-label='Apagar todo o historico'
+                title='Apagar historico'
+              />
             </span>
           </Heading>
         </Container>
 
         <Container>
-          <div className='resposiveTable'>saddadasdadsda</div>
+          <div className={styles.responsiveTable}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Tarefa</th>
+                  <th>Duração</th>
+                  <th>Data</th>
+                  <th>Status</th>
+                  <th>Tipo</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Array.from({ length: 20 }).map((_, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>Estudar</td>
+                      <td>25 min</td>
+                      <td>20/04/2025 08:00</td>
+                      <td>Completa</td>
+                      <td>Foco</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </Container>
       </MainTemplate>
     </>
