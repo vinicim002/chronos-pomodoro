f218ccb76dd676b88b6dc956d5ccb09ade9d680b
Vinicius Silva <viniciusvilanova7@gmail.com>
2025-12-18 17:26:39 -0300

    Revisar aula

--- src/contexts/TaskContext/taskReducer.ts
+++ src/contexts/TaskContext/taskReducer.ts
@@ -1,6 +1,7 @@
 import type { TaskStateModel } from '../../models/TaskStateModel';
 import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 import { getNextCycle } from '../../utils/getNextCycle';
+import { initialTaskState } from './initialTaskState';
 import { TaskActionTypes, type TaskActionModel } from './taskAction';
 
 
@@ -52,7 +53,9 @@ export function taskReducer(
       };
     }
     case TaskActionTypes.RESET_STATE: {
-      return state;
+      return {
+        ...initialTaskState
+      };
     }
     case TaskActionTypes.COUNT_DOWN: {
       return {
